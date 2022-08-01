a18ac52e2044874e372390230b00f88c210996f8
Parham Javadi <parham@Parhams-MacBook-Pro.local>
2022-08-01 09:34:05 -0700

    updated portfolio

--- build/_next/static/chunks/webpack.js
+++ build/_next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0385e2d2c9738034c839"; }
+/******/ 		__webpack_require__.h = function() { return "8399bccf1ed1ed403b0a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
