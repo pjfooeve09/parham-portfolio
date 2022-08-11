d9eb352438db1c65acc119b6af025d6f6ddae22b
Parham Javadi <parham@Parhams-MacBook-Pro.local>
2022-08-11 10:51:07 -0700

    updated resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = (props) => (
         <SectionText>
           My name is Parham Javadi and I am a Full Stack Software Developer.
         </SectionText>
-        <Link href="https://drive.google.com/file/d/1_F0rHecBE_l1tv6JbQq-xhFgCEEfz5FB/view?usp=sharing" target="_blank"> 
+        <Link href="https://drive.google.com/file/d/1DXtdRRbRl2NqLjE7tq2iYQJdE5KMCGIT/view?usp=sharing" target="_blank"> 
           <h1>My Resume</h1>
         </Link>
       </LeftSection>
