741e89e0c0a960e48f2e02cf22e7148808b647a1
Parham Javadi <parham@Parhams-MacBook-Pro.local>
2022-08-24 12:42:02 -0700

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "caa470f2a981aeffebe5"; }
+/******/ 		__webpack_require__.h = function() { return "90e311f7389fd09e9856"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- _next/static/webpack/webpack.0fbeac636b9e932a08d5.hot-update.js
+++ _next/static/webpack/webpack.0fbeac636b9e932a08d5.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ac63320d32f2ef346f6d"; }
+/******/ 	__webpack_require__.h = function() { return "f3ce05668b3df69f3e18"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
